90e4506e9d3be4bbb9ca6c471278cd9ad7f153a7
Axel HEINE <axheine@gmail.com>
2016-10-26 17:15:13 +0200

    Add some ideas for Users storage

--- src/papayaDB/user/User.java
+++ src/papayaDB/user/User.java
@@ -3,5 +3,8 @@ package papayaDB.user;
 import papayaDB.api.QueryInterface;
 
 public class User implements QueryInterface {
-
+    /*
+    * Les comptes des utilisateurs peuvent être stockés dans une base
+    * prévue créer à l'installation de PapayaDB, elle serait non supprimable et invisible pour les non root.
+    */
 }
