a2773fc1477ed772b9108b44838ad7be6d9f2a4f
Genroa <genroa@hotmail.fr>
2016-11-02 21:18:25 +0100

    Doc

--- src/papayaDB/api/chainable/AbstractChainableQueryInterface.java
+++ src/papayaDB/api/chainable/AbstractChainableQueryInterface.java
@@ -4,7 +4,9 @@ import papayaDB.api.QueryInterface;
 
 /**
  * Cette classe abstraite implémente les concepts communs aux "interfaces chainées" : des interfaces de requête pouvant traiter celles qu'elles reçoivent en les renvoyant vers d'autres (ou pas).
+ * On peut considérer cette classe comme la classe la plus adaptée à étendre pour créer ses propres QueryInterface.
+ * Cette implémentation définit le fonctionnement d'une AbstractChainableQueryInterface.
  */
 public class AbstractChainableQueryInterface implements QueryInterface {
-
+	
 }
