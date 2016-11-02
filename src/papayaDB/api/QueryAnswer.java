af6012775d250e811d32a13ab6d203af9307a240
Axel HEINE <axheine@gmail.com>
2016-11-02 23:21:12 +0100

    JavaDoc Comments

--- src/papayaDB/api/QueryAnswer.java
+++ src/papayaDB/api/QueryAnswer.java
@@ -2,10 +2,24 @@ package papayaDB.api;
 
 import io.vertx.core.json.JsonObject;
 
+/**
+ * Contien l'Objet Json de réponse et le code de status.
+ * Le code de status es contenu aussi dans l'objet JSON, il est présent dans la classe pour des raisons de confort.
+ */
 public class QueryAnswer {
+	/**
+	 * Objet JSON renvoyé.
+	 */
 	private JsonObject answer;
+	/**
+	 * Status de la réponse.
+	 */
 	private QueryAnswerStatus status;
 	
+	/**
+	 * Constructeur de la {@link QueryAnswer}
+	 * @param answer La réponse qui sera contenue dans l'objet.
+	 */
 	public QueryAnswer(JsonObject answer) {
 		this.answer = answer;
 		this.status = QueryAnswerStatus.OK;
--- src/papayaDB/api/chainable/AbstractChainableQueryInterface.java
+++ src/papayaDB/api/chainable/AbstractChainableQueryInterface.java
@@ -12,21 +12,36 @@ import papayaDB.api.QueryInterface;
  * Cette implémentation définit le fonctionnement d'une AbstractChainableQueryInterface.
  */
 public abstract class AbstractChainableQueryInterface implements QueryInterface {
+	/**
+	 * Objet {@link Vertx}
+	 */
 	private final Vertx vertx;
 	
+	/**
+	 * Constructeur de la classe abstraite.
+	 * Créé un Objet {@link Vertx} qui sera stocké. 
+	 */
 	public AbstractChainableQueryInterface() {
 		vertx = Vertx.vertx();
 	}
-	
+
 	@Override
-	public void processQuery(String query,Consumer<QueryAnswer> callback) {
+	public void processQuery(String query, Consumer<QueryAnswer> callback) {
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Ferme définitivement les connexions et rend l'objet inutilisable. A appeler impérativement après utilisation.
+	 * En cas d'oubli, les threads de Vertx maintiendront la JVM en vie.
+	 */
 	public void close() {
 		vertx.close();
 	}
 	
+	/**
+	 * Méthode permettant de récuperer l'objet {@link Vertx} instancié.
+	 * @return Vertx L'objet {@link Vertx}
+	 */
 	public Vertx getVertx() {
 		return vertx;
 	}
