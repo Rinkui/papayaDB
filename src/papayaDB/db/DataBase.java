d1f62160cdd92d8aa1358338b89e1a8552d817b2
Thibault <Thibault@Thibault-PC>
2016-11-05 19:13:51 +0100

    Initial Commit

---
+++ src/papayaDB/db/DataBase.java
@@ -0,0 +1,10 @@
+package papayaDB.db;
+
+public class DataBase<T> {
+	private DataBase<T> db;
+	private 
+	
+	public DataBase(){
+		db = new DataBase<>();
+	}
+}
---
+++ src/papayaDB/db/Optimisateur.java
@@ -0,0 +1,47 @@
+package papayaDB.db;
+
+import java.util.Objects;
+
+public class Optimisateur {
+	private Node root;
+	
+	
+	
+	
+	class Node{
+		final String filterName;
+		final String filterValue;
+		Node[] link;
+		
+		Node(String name, String value){
+			Objects.requireNonNull(name);
+			Objects.requireNonNull(value);
+			this.filterName = name;
+			this.filterValue = value;
+		}
+		
+		Node(){
+			
+		}
+		
+		@Override
+		public int hashCode() {
+			return filterName.hashCode()^filterValue.hashCode();
+		}
+		
+		public boolean equals(Object obj){
+			if(obj == null){
+				return false;
+			}
+			if(this == obj){
+				return true;
+			}
+			if(!(obj instanceof Node)){
+				return false;
+			}
+			Node n = (Node)obj;
+			return filterName.equals(n.filterName) && filterValue.equals(n.filterValue);
+		}
+		
+	}
+}
