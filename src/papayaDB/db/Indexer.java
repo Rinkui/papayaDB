0decb6b663c924f462b2bf25cd02776401af96da
Rinkui <Rinkui@DESKTOP-QVAFHIR>
2016-11-24 17:57:04 +0100

    on réfléchie on réfléchie je commit parce que je vais chez thomas

--- src/papayaDB/db/Indexer.java
+++ src/papayaDB/db/Indexer.java
@@ -11,46 +11,16 @@ import java.util.Objects;
 import jdk.internal.reflect.ReflectionFactory.GetReflectionFactoryAction;
 
 public class Indexer {
-	// gere les index
-	// deux fichiers par type d'objet:
-	// - un avec les index
-	// les indexs seront stockés en int donc un index = 4 octets
-	// * première ligne, les index des champs
-	// * le reste, chaque int est un index d'un objet
-	// - un avec toutes les données présentées comme suit :
-	// seul la taille en octet est stockée en int, le reste est stocké en
-	// char/string
-	// * première ligne : nom du type des objets qui suivront
-	// * seconde ligne : nombre de champs puis tous les champs avec la taille en
-	// octet devant chaque
-	// champs, un index par champs qui est au niveau du int
-	// * le reste des lignes : une ligne par objet avec seulement les valeurs,
-	// la taille en octet devant chaque valeur, un index par champs qui se
-	// trouve au niveau de l'int
 
-	private static final int MARGIN = 100; // la marge que l'on choisi de
-											// laisser pour l'ajout d'objets
-	// pour les deux tableaux on retient l'index de la taille du champs
-	// il faudrait un équivalent nom du champs : place dans le tableau
-	private int[] fieldsIndex; // indexs des champs
-	private int[] objectsIndex; // indexs des objets (les indexs que l'on trouve
-								// dans le fichier de int)
-	private String type;
-	private Reader reader; // je pars du principe que rien n'a été initialisé
 
-	// ça me plait pas du tout du tout mais je sias pas comment faire
-	// surement la découpe de mes classes qui est fausse
-	public Indexer(Reader reader) {
-		this.reader = reader;
-		this.type = this.reader.firstTypeReading();
-		this.fieldsIndex = this.reader.firstFieldsReading();
-		this.objectsIndex = this.reader.firstObjectsReading(this.fieldsIndex.length);
-	}
+	
+	
+	
+
 
 	public int[] getIndex(int firstIndex) {
 		// on vérifie que l'index donné est dans le tableau et qu'il reste le
 		// bon nombre de champs
-		Objects.checkIndex(firstIndex, objectsIndex.length - fieldsIndex.length);
 		return Arrays.copyOfRange(objectsIndex, firstIndex, firstIndex + fieldsIndex.length);
 	}
 
@@ -60,10 +30,6 @@ public class Indexer {
 		// puis appeler le reader avec l'index de début du champs
 		// enfin retourne la string renvoyée par le reader
 		return "";
-	}
-	
-	public String getType() {
-		return type;
 	}
 
 	// pour avoir une map :
--- src/papayaDB/db/Reader.java
+++ src/papayaDB/db/Reader.java
@@ -1,81 +1,112 @@
 package papayaDB.db;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
+
+// gere les index
+// deux fichiers par type d'objet:
+// - un avec les index
+// les indexs seront stockés en int donc un index = 4 octets
+// * première ligne, les index des champs
+// * le reste, chaque int est un index d'un objet
+// - un avec toutes les données présentées comme suit :
+// seul la taille en octet est stockée en int, le reste est stocké en
+// char/string
+// * première ligne : nom du type des objets qui suivront
+// * seconde ligne : nombre de champs puis tous les champs avec la taille en
+// octet devant chaque
+// champs, un index par champs qui est au niveau du int
+// * le reste des lignes : une ligne par objet avec seulement les valeurs,
+// la taille en octet devant chaque valeur, un index par champs qui se
+// trouve au niveau de l'int
 
 public class Reader {
-	private MappedByteBuffer map;
-	private int firstFieldPos;
-	private int nbObjectsPos;
+	private static final int MARGIN = 100; // la marge que l'on choisi de
+	// laisser pour l'ajout d'objets
 
-	public Reader(MappedByteBuffer map){
+	private MappedByteBuffer map;
+
+	// pour les deux tableaux on retient l'index de la taille du champs
+	// à changer : on stocke le nom des champs
+	private String[] fieldsNames = null; // indexs des champs
+	private int[] fieldsSizes = null; // taille max des champs
+	private int[] objectsIndex = null; // indexs des objets (les indexs que l'on trouve
+								// dans le fichier de int)
+	private String type = null;
+
+	public Reader(MappedByteBuffer map) {
 		this.map = map;
 	}
 
-	// on doit lire une fois en entier la première fois pour retenir les index
-
-	// on remet la position à 0 au cas ou quelqu'un d'intelligent essaye de
-	// faire plusieurs fois cette fonction
-	public String firstTypeReading() {
+	private void firstTypeReading() {
 		char c;
 		StringBuilder sb = new StringBuilder();
 		map.position(0);
 		while ((c = map.getChar()) != '\n') {
 			sb.append(c);
 		}
-		firstFieldPos = map.position();
-		return sb.toString();
+		type = sb.toString();
 	}
-	
-	private int[] loopReading(int[] index){
+
+	private void firstFieldsReading() {
+		if(type == null)
+			firstTypeReading();
+		int nbFields = map.getInt();
+		fieldsNames = new String[nbFields];
+		fieldsSizes = new int[nbFields];
 		int nbChar;
-		for (int i = 0; i < index.length; i++) {
-			index[i] = map.position();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < nbFields; i++) {
+			fieldsSizes[i] = map.getInt();
+			nbChar = map.getInt();
+			for (int j = 0; j < nbChar; j++) {
+				map.getChar();
+			}
+			fieldsNames[i] = sb.toString();
+			sb.setLength(0);
+		}
+	}
+
+	private void firstObjectsReading(int nbFields, int margin) {
+		if(objectsIndex == null)
+			firstFieldsReading();
+		int nbObjects = map.getInt();
+		objectsIndex = new int[nbObjects + margin];
+		int nbChar;
+		for (int i = 0; i < nbObjects; i++) {
+			objectsIndex[i] = map.position();
 			nbChar = map.getInt();
 			for (int j = 0; j < nbChar; j++) {
 				map.getChar();
 			}
 		}
-		return index;
-	}
-
-	// a nouveau on replace le curseur au bon endroit
-	public int[] firstFieldsReading() {
-		if (firstFieldPos == 0)
-			throw new IllegalStateException("Type non initialisé");
-		map.position(firstFieldPos);
-		int nbFields = map.getInt();
-		int[] fieldsIndex = new int[nbFields];
-		fieldsIndex = loopReading(fieldsIndex);
-		nbObjectsPos = map.position();
-		return fieldsIndex;
-	}
-
-	public int[] firstObjectsReading(int nbFields) {
-		if (nbObjectsPos == 0)
-			throw new IllegalStateException("Champs non initialisés");
-		map.position(nbObjectsPos);
-		int nbObjects = map.getInt();
-		int[] objectsIndex = new int[nbObjects];
-		return loopReading(objectsIndex);
 	}
 	
-	public String getFieldValue(int firstIndex){
-		StringBuilder sb = new StringBuilder();
-		map.position(firstIndex);
-		int size = map.getInt();
-		for(int i = 0 ; i < size ; i ++ ){
-			sb.append(map.getChar());
+	private int getFieldIndex(String fieldName){
+		for(int i = 0 ; i < fieldsNames.length ; i ++){
+			if( fieldName.equals(fieldsNames[i]) )
+				return i;
 		}
-		return sb.toString();
+		return -1;
+	}
+	
+	public String getFieldValue(int objectIndex, String fieldName){
+		if(fieldsNames == null)
+			firstFieldsReading();
+		int indexField;
+		if( (indexField = getFieldIndex(fieldName)) == -1 )
+			return "";
+		
+		return "";
 	}
 
 	// pour récuperer une donnée de map : il faut placer le curseur au bon
 	// endroit index avec map.position(index), puis faire un map.get(byte[],
 	// offset, length) pour
 	// récuperer tout le contenu, attention, le curseur sera à la fin
+
+	// pour avoir une map :
+	// FileInputStream in = new FileInputStream(file);
+	// FileChannel fc = in.getChannel();
+	// this.map = fc.map(MapMode.READ_WRITE, 0, fc.size());
+	// in.close();
 }
