55499eed26b3bfb5614189de754fd58c4f91dc80
Thibault <Thibault@Thibault-PC>
2016-11-27 17:51:38 +0100

    Commencement des regex pour des coupages

--- src/papayaDB/server/TestServer.java
+++ src/papayaDB/server/TestServer.java
@@ -1,8 +1,11 @@
 package papayaDB.server;
 
+import java.util.Arrays;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -14,9 +17,9 @@ public class TestServer extends AbstractVerticle{
 	    Router router = Router.router(vertx);
 	    
 	    // route to JSON REST APIs
-	    router.get("/test").handler(routingContext -> this.test(routingContext,router));
-	    router.get("/all").handler(routingContext -> this.test(routingContext,router));
-	    router.get("/get/:name/:id").handler(routingContext -> this.test(routingContext,router));
+	    router.get("/*").handler(this::test);
+//	    router.get("/all").handler(routingContext -> this.test(routingContext,router));
+//	    router.get("/get/:name/:id").handler(routingContext -> this.test(routingContext,router));
 	    
 	    // otherwise serve static pages
 	    router.route().handler(StaticHandler.create());
@@ -25,17 +28,24 @@ public class TestServer extends AbstractVerticle{
 	    System.out.println("listen on port 8080");
 	  }
 	  
-	  private void test(RoutingContext routingContext, Router router){
-		  HttpServerRequest r = routingContext.request();
-		  System.out.println(r.params().names());
-		  for(String name : r.params().names()){
-			  System.out.println(name + ": " + r.getParam(name));
-			  routingContext.reroute(r.getParam(name));
+	  private void test(RoutingContext routingContext){
+		  HttpServerResponse response = routingContext.response();
+		  HttpServerRequest request = routingContext.request();
+		  String path = request.path().substring(1).replaceAll("%22", "\"").replaceAll("%20", " ");
+		  System.out.println(path);
+		  String[] pathCut = path.split("/");
+		  System.out.println(Arrays.toString(pathCut));
+		  if(pathCut.length%2 == 0){
+			  response.setStatusCode(400).end("Request is not correct.");
 		  }
-		  for(Route route : router.getRoutes()){
-			  System.out.println(route.getPath());
+		  else{
+			  response.setStatusCode(200).end("Request: " + path);
 		  }
-		  System.out.println("END LIST");
+//		  System.out.println(request.params().names());
+//		  for(String name : request.params().names()){
+//			  System.out.println(name + ": " + request.getParam(name));
+//		  }
+		  System.out.println("END PARAM");
 	  }
 
 
